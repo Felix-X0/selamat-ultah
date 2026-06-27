4ba51d3a5e1bcde58016ad025eb3c51db8a64aa9
felixxx_X0 <48359859+Felix-X0@users.noreply.github.com>
2026-06-28 02:38:04 +0700

    Create App component with birthday greeting

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        background:
+          "linear-gradient(135deg,#3b0000,#8b0000,#c1121f)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        color: "#fff",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1 style={{fontSize:"55px",margin:0}}>
+        🎉 Happy Birthday 🎉
+      </h1>
+
+      <p style={{fontSize:"22px"}}>
+        A Special Letter is Waiting For You ❤️
+      </p>
+    </div>
+  );
+}
